235e0513bdb625d5777b2eb3baaa555d2eaadccc
KHVIII <ytc344@nyu.edu>
2020-06-22 09:29:58 -0700

    added untested register page with bcrypt

--- mySQL_test/mysql_test.js
+++ mySQL_test/mysql_test.js
@@ -3,6 +3,7 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
+var bcrypt = require ("bcrypt");
 
 
 const connection = mysql.createConnection({
@@ -20,18 +21,79 @@ connection.connect((err) => {
 
 var app = express();
 app.use(session({
-	secret: 'secret',
+	secret: 'secret', //change this to some complicated key upon realisation
 	resave: true,
 	saveUninitialized: true
 }));
 
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //using express and bodyParser as middleware, serialise incoming request as .json data
 
-app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/test_page.html'));
+app.get('/', function(request, response) { //at localhost:3000/
+	//response.sendFile(path.join(__dirname + '/test_page.html'));           //this is to directly login page
+	response.sendFile(path.join(__dirname + '/register.html'));				//this is to register page first
+}); 
+
+	//register form with bcrypt
+app.post('/register', function(request, response) {
+	const password = request.body.password;
+	const encryptedPassword = awaitbcrypt.hash (password, saltRounds)
+
+	var accounts={
+		"username":request.body.username,
+		"password":encryptedPassword,
+		"email":"test"
+	}
+
+	connection.query('INSERT INTO accounts SET ?', accounts, function (error, results, fields) {
+		if (error) {
+			response.send('Something went wrong');
+			esponse.end();
+		} else {
+			response.send('succesfully registered');
+			response.sendFile(path.join(__dirname + '/test_page.html'));
+		}	
+	});
 });
 
+	//login form with bcrypt
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ?', [username], function(error, results, fields) {
+			if (error) {
+				response.send('Something went wrong');
+			} else {
+				if (results.length > 0) {
+					const comparison = await bcrypt.compare(password, results[0].password)
+					if (comparison) {
+						request.session.loggedin = true;
+						request.session.username = username;
+						response.redirect('/home')
+					} 
+					else {
+						response.send('Username and password does not match');
+					}
+				} 
+				else {
+					response.send('Username does not exist');
+				}			
+				response.end();
+			}});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+
+
+
+
+
+/*    
+	//Login form without bcrypt
 app.post('/auth', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
@@ -51,6 +113,8 @@ app.post('/auth', function(request, response) {
 		response.end();
 	}
 });
+*/
+
 
 app.get('/home', function(request, response) {
 	if (request.session.loggedin) {
